f6d4252c7141e54f2a4ca006a2dd9075e825042f
bbu0810 <bbu0810@outlook.com>
2018-10-09 16:54:53 +1000

    version1.0.10.9

---
+++ app/src/main/java/elisompo/com/elisompo/Interface/GlobalConstants.java
@@ -0,0 +1,10 @@
+package elisompo.com.elisompo.Interface;
+
+public class GlobalConstants {
+    public static String sPHONE = "";
+    public static String sTEXT = "";
+    public static String sIMAGE = "";
+    public static String sJOB_ID = "";
+    public static String sSLEEP = "";
+    public static String sIMAGE_ABSOLUTION_PATH = "";
+}
