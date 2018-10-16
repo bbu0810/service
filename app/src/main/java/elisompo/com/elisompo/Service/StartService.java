8c5048eb2f7d6c2718fd886a4db26a624da11c7e
bbu0810 <bbu0810@outlook.com>
2018-10-17 03:14:22 +1000

    versio1.0.10.17

---
+++ app/src/main/java/elisompo/com/elisompo/Service/StartService.java
@@ -0,0 +1,14 @@
+package elisompo.com.elisompo.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class StartService extends Service{
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
