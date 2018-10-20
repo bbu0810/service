28912850c1e074da4a58d6ac2fe78ca8c3b8a4c3
bbu0810 <bbu0810@outlook.com>
2018-10-21 01:38:24 +0300

    version1.0.10.21

--- app/src/main/java/elisompo/com/elisompo/Interface/AAInterface.java
+++
@@ -1,9 +0,0 @@
-package elisompo.com.elisompo.Interface;
-
-import org.json.JSONObject;
-
-public interface AAInterface {
-    void getOrderListSuccess(String resphoneMessage);
-    void getOrderListFail(String resphoneMessage);
-    void getOrderListError();
-}
